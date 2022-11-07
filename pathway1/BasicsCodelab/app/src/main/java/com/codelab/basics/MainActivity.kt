343fb919fa73e346c2019535577764a9ca117cc1
Younggun, An <boinred@outlook.com>
2022-11-07 18:09:56 +0900

    ✨ Completed : 4.Lists and animations

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.codelab.basics
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -28,7 +31,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Message(val author: String, val body: String)
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message = message)
+        }
+    }//: LazyColumn
+}
+
 
 @Composable
 fun MessageCard(message: Message) {
@@ -50,31 +61,47 @@ fun MessageCard(message: Message) {
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
+        var isExpanded by remember { mutableStateOf(false) }
 
-        Column {
+        Column(
+            modifier = Modifier.clickable { isExpanded = !isExpanded },
+        ) {
             Text(
                 text = message.author,
-                color = MaterialTheme.colorScheme.secondary
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = message.body)
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp
+            ) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }//: Column    
     }//: Row
 
 }
 
 @Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
 @Composable
 fun PreviewMessageCard() {
     BasicsCodelabTheme {
-        MessageCard(
-            message = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-        )
+//        MessageCard(
+//            message = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+//        )
+        
+        Conversation(messages = SampleData.conversationSample)
     }//: BasicsCodelabTheme
 
 }
