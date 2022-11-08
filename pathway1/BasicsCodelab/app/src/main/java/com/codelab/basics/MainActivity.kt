3d8d5938bf98fa6efb535e2902a46a9194b8e464
Younggun, An <boinred@outlook.com>
2022-11-08 20:12:41 +0900

    ✨ Completed : 첫 번째 Compose 앱 만들기

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,107 +1,132 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            BasicsCodelabTheme {
+                MyApp()
+            }
         }
     }
 }
 
 @Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message = message)
-        }
-    }//: LazyColumn
+private fun MyApp() {
+//    var shouldShowOnboard by remember { mutableStateOf(true) }
+    var shouldShowOnboard by rememberSaveable {
+        mutableStateOf(true)
+    }
+    if (shouldShowOnboard) {
+        OnboardScreen(onContinueClicked = {
+            shouldShowOnboard = false
+        })
+    } else {
+        Greetings()
+    }
 }
 
-
 @Composable
-fun MessageCard(message: Message) {
-    // Add padding around our message
-    Row(
-        modifier = Modifier.padding(all = 8.dp)
+fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
+    )
+
+    Surface(
+        color = MaterialTheme.colorScheme.secondary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                // Set image size to 40 dp
-                .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-        )
-
-        // Add a horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(8.dp))
-
-        var isExpanded by remember { mutableStateOf(false) }
-
-        Column(
-            modifier = Modifier.clickable { isExpanded = !isExpanded },
-        ) {
-            Text(
-                text = message.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleSmall
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                shadowElevation = 1.dp
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
-                Text(
-                    text = message.body,
-                    modifier = Modifier.padding(4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyMedium
+                Text(text = "Hello, ")
+                Text(text = name, style = MaterialTheme.typography.titleLarge)
+
+                if (expanded.value) {
+                    Text(text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                }
+            }//: Column
+
+            IconButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Icon(
+                    imageVector = if (expanded.value) Icons.Default.ExpandLess else Icons.Default.ExpandMore,
+                    contentDescription = if (expanded.value) "Show less" else "Show more"
                 )
             }
-        }//: Column    
-    }//: Row
-
+        }//: Row
+    }//: Surface
 }
 
-@Preview(name = "Light Mode")
-//@Preview(
-//    uiMode = Configuration.UI_MODE_NIGHT_YES,
-//    showBackground = true,
-//    name = "Dark Mode"
-//)
 @Composable
-fun PreviewMessageCard() {
-    BasicsCodelabTheme {
-//        MessageCard(
-//            message = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-//        )
-        
-        Conversation(messages = SampleData.conversationSample)
-    }//: BasicsCodelabTheme
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    } // LazyColumn
+}
 
+@Composable
+fun OnboardScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Code-lab!")
+            Button(onClick = onContinueClicked, modifier = Modifier.padding(vertical = 24.dp)) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
 }
